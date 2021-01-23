f8e82a4a8a1afb50619a220b88256c9bc0390f95
AmirHosseinCV <bagheri.810907@gmail.com>
2021-01-23 18:32:34 +0100

    bug fixes

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='dvc',
     version='0.3.0',
     scripts=["bin/dvc.bat"],
+    packages=find_packages(),
     py_modules=["dvc"],
     description='fast and easy to use data version controller',
     author='AmirCV',
