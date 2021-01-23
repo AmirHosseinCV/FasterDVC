1bdd015c422963dd534a48ba93e849598830de74
AmirHosseinCV <bagheri.810907@gmail.com>
2021-01-23 18:28:43 +0100

    bug fixes

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='dvc',
     version='0.3.0',
     scripts=["bin/dvc.bat"],
+    py_modules=["dvc"],
     description='fast and easy to use data version controller',
     author='AmirCV',
     license='MIT'
