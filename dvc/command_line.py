5a583ff4fb8befac6a9208d7081e3f914dda5d63
AmirHosseinCV <bagheri.810907@gmail.com>
2021-01-26 07:20:10 +0100

    command line problems solved

---
+++ dvc/command_line.py
@@ -0,0 +1,29 @@
+import sys
+import glob
+import os
+import json
+import dvc
+
+
+def main():
+    if sys.argv[1] == "commit":
+        dvc.commit(sys.argv[2])
+    elif sys.argv[1] == "restore":
+        dvc.restore(int(sys.argv[2]))
+    elif sys.argv[1] == "ls":
+        versions = glob.glob(os.path.join(dvc.versionsPath, "*.txt"))
+        for vf in versions:
+            v = json.loads(open(vf, 'r').read())
+            vf = os.path.basename(vf).replace(".txt", "")
+            print(vf, '\t', v["note"])
+        v = input("restore? ([n] or version number): ")
+        if v == "n":
+            exit()
+        try:
+            dvc.restore(int(v))
+        except Exception as e:
+            print(e)
+    elif sys.argv[1] == "init":
+        dvc.init()
+    elif sys.argv[1] == "clear":
+        dvc.clear()
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='dvc',
-    version='0.3.0',
-    scripts=["bin/dvc.bat"],
+    version='0.5.0',
+    entry_points={
+        'console_scripts': ['dvc=dvc.command_line:main'],
+    },
     packages=["dvc"],
     description='fast and easy to use data version controller',
     author='AmirCV',
